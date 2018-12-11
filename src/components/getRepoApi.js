c95347268a851af18e3a88bb4c3f2fd9dea506b9
Shalom <oyeniranexcellenced@gmail.com>
2018-12-11 13:02:07 +0100

    Imported Eslint into package.json and created getRepoApi.js that makes a get call to github API

--- src/components/getRepoApi.js
+++ src/components/getRepoApi.js
@@ -1,10 +1,11 @@
 import axios from 'axios'
 import {AsyncStorage} from "AsyncStorage"
 
-export const apiGetter = async (inputVal) => {
-  console.log(inputVal)
-  const resp = await axios
-    .get(`https://api.github.com/search/repositories?q=${inputVal}`)
-
-    return resp;
-}
+        export const apiGetter = async (inputVal) => {
+          //inputVal is passed as a parameter from 'search.js'
+          //console.log(inputVal)
+          const resp = await axios
+            .get(`https://api.github.com/search/repositories?q=${inputVal}`)
+              //store the data from the get method into 'resp' and return 'resp'
+            return resp;
+        }
