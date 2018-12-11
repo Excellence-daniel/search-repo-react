8d219fa1a5840a305f23ce1b97158db375798710
Shalom <oyeniranexcellenced@gmail.com>
2018-12-11 12:55:53 +0100

    using the parameter sent from search.js, call data from api and return response

--- src/components/getRepoApi.js
+++ src/components/getRepoApi.js
@@ -1,16 +1,10 @@
 import axios from 'axios'
 import {AsyncStorage} from "AsyncStorage"
 
-
-
-
-
-
-
 export const apiGetter = async (inputVal) => {
   console.log(inputVal)
   const resp = await axios
     .get(`https://api.github.com/search/repositories?q=${inputVal}`)
-    
+
     return resp;
 }
